670eac81175651e1991d7b28ca9cf1690d9f24a1
David <david48kariuki@gmail.com>
2025-03-05 15:55:43 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+razzle();
+function razzle() {
+    console.log("You've been razzled!");
+}
+
+function razzle(lawyer = "Billy", target = "'em") {
+  console.log(`${lawyer} razzle-dazzles ${target}!`);
+}
+razzle(); 
+razzle("Methuselah", "T'challah");
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(wrapper) {
+  return function(adjective) {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
